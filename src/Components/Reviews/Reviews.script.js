82d5e35612fafdd75de2a2d35d54e9ff51f6f55e
EECvision <eec.studies@gmail.com>
2022-11-21 23:39:30 +0100

    update reviews

--- src/Components/Reviews/Reviews.script.js
+++ src/Components/Reviews/Reviews.script.js
@@ -1,17 +1,17 @@
 export const REVIEWS = [
   {
     review:
-      "That's why I create digital designs that helps create the connections between your business and customers by combining research, designs and creative ideas into a beautiful and usable products.",
+      "Buchi is one of the brightest minds I have ever had the privilege to work with. He is extremely creative and his ability to consistently produce better work has always impressed me. He is an excellent Designer and will make an excellent addition to any team.",
     reviewer: "~Micheal  Adewale~",
   },
   {
     review:
-      "That's why I create digital designs that helps create the connections between your business and customers by combining research, designs and creative ideas into a beautiful and usable products.",
-    reviewer: "~Nafiza Efina~",
+      "I worked with Buchi for the past year, he is an excellent UI/UX Designer and interacts with others in a professional way. I highly recommend him to any company.",
+    reviewer: "~Mohamed ElKhamisy~",
   },
   {
     review:
-      "That's why I create digital designs that helps create the connections between your business and customers by combining research, designs and creative ideas into a beautiful and usable products.",
+      "Buchi is a great designer. He is one of the kindest people I have worked it. On top of that, he is highly skilled and creative. If there is anything as rating someone 100%, I will rate him that.",
     reviewer: "~Emmanuel Ezeka~",
   },
 ]
